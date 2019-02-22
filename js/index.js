fab1fe7c43cb62d02c826b804be428666ab0f991
lzzhhh <419193061@qq.com>
2019-02-22 17:50:34 +0800

    考试

---
+++ js/index.js
@@ -0,0 +1,52 @@
+
+//   
+$(".lili1").mouseover(function(){
+    $(".cc").stop().animate({bottom:'29px'},200);
+  }); 
+  $(".lili1").mouseout(function(){
+    $(".cc").stop().animate({bottom:'-45px'},200);
+  }); 
+
+  $(".lili2").mouseover(function(){
+    $(".cc2").stop().animate({bottom:'51px'},200);
+  }); 
+  $(".lili2").mouseout(function(){
+    $(".cc2").stop().animate({bottom:'-22px'},200);
+  }); 
+
+  $(".lili3").mouseover(function(){
+    $(".cc3").stop().animate({bottom:'51px'},200);
+  }); 
+  $(".lili3").mouseout(function(){
+    $(".cc3").stop().animate({bottom:'-22px'},200);
+  }); 
+
+  $(".lili4").mouseover(function(){
+    $(".cc4").stop().animate({bottom:'29px'},200);
+  }); 
+  $(".lili4").mouseout(function(){
+    $(".cc4").stop().animate({bottom:'-46px'},200);
+  }); 
+
+  $(".lili5").mouseover(function(){
+    $(".cc5").stop().animate({bottom:'59px'},200);
+  }); 
+  $(".lili5").mouseout(function(){
+    $(".cc5").stop().animate({bottom:'-16px'},200);
+  }); 
+
+  $(".lili6").mouseover(function(){
+    $(".cc6").stop().animate({bottom:'29px'},200);
+  }); 
+  $(".lili6").mouseout(function(){
+    $(".cc6").stop().animate({bottom:'-46px'},200);
+  }); 
+
+  $(".lili7").mouseover(function(){
+    $(".cc7").stop().animate({bottom:'29px'},200);
+  }); 
+  $(".lili7").mouseout(function(){
+    $(".cc7").stop().animate({bottom:'-46px'},200);
+  }); 
+
+ 
